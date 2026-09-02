362079017f480d566571afd4a236e6177052245c
SohairNasir <sohairarain002@gmail.com>
2026-09-02 12:37:07 +0500

    Allhumdullilah about page work 70% complete

--- src/components/GmwAbout/GmwAbout.jsx
+++ src/components/GmwAbout/GmwAbout.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import './GmwAbout.css'
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
 
 const GmwAbout = () => {
   return (
     <main className="!pt-[60px]  !pb-[60px] !pl-[99px] !pr-[99px]">
-      <div className="child-wrapper">
-
+     
+      <div className="child-wrapper flex  justify-between">
+         
         <section className="w-[100%] max-w-[527px]">
-          <h1>gmw</h1>
+         
+          <h1 className="!mb-[65px]"><strong className="brand-heading">gwm</strong></h1>
           
-          <p>
+          <p className="gmw-parah">
             At GWM, innovation, sustainability, and quality aren't just values —
             they're a belief shared by every employee, in every market, every
             day. It's what has driven us since 1990, constantly pushing the
@@ -19,11 +23,14 @@ const GmwAbout = () => {
             very first drive
           </p>
 
+          <button className="button-and-arrow">Find out more < MdOutlineKeyboardArrowRight size={'25px'}/></button>
+
         </section>
 
         {/* left div */}
 
-        <div></div>
+        <div className="gwm-img-right">
+        </div>
       
       </div>
     </main>
