5bc57b77fb2d6e6a0f49b9277b6d0ec9f8afb762
SohairNasir <sohairarain002@gmail.com>
2026-09-04 22:49:32 +0500

    pending work Test Drive page in (Your Details) section on input alignment

--- src/pages/TestDrive/TestDrive.jsx
+++ src/pages/TestDrive/TestDrive.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import'./TestDrive.css'
+import DownSvg from'../../assets/chevronDown.svg?react'
 const TestDrive = () => {
   return (
+
     <main className='flex items-center justify-center'>
         <div className='w-full  max-w-[800px]'>
 
@@ -11,18 +13,51 @@ const TestDrive = () => {
                 <p className='border-line'></p>
             </div>
 
-            <div className='flex gap-5 '>
-                <h6>
+            <div className='flex gap-5 w-full justify-between'>
+                
+                <span className='inpt-title'>
                     Select a Vehicle
-                </h6>
+                </span>
 
-                <div className='vehicle-select-con'>
-                    <select name="" id="">
-                        <option value="">hey    </option>
-                    </select>
+                <div className='vehicle-select-con !p-[12px] flex justify-between'>
+                    
+                    <div className='flex gap-[15px] items-center'>
+                    
+                       <div className='image-con grid text-gray-500 place-items-center'>
+                        {/* <img src="" alt="" /> */}
+                        Select Vehicle
+                       </div>
+                     <h6>Select Vehicle</h6>
+                    
+                    </div>
+
+                    {/* down chevron */}
+
+                    <DownSvg className='w-[20px]'/>
                 </div>
                 
             </div>
+
+            <div className='flex justify-center !mb-[8px] !mt-[25px]'>
+                <p className='border-line'></p>
+            </div>
+
+
+            <div className='flex gap-5px w-full justify-between'>
+                <span className='inpt-title'>
+                    Your Details
+                </span>
+
+                <div className='flex flex-wrap gap-[8px] w-full max-w-[500px]'>
+                    <div className="info-inpt-con"><span>svg</span><input type="text" /></div>
+                    <div className="info-inpt-con"><span>svg</span><input type="text" /></div>
+                    <div className="info-inpt-con"><span>svg</span><input type="text" /></div>
+                    <div className="info-inpt-con"><span>svg</span><input type="text" /></div>
+                </div>
+
+
+            </div>
+
 
         </div>
     </main>
